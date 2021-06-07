b3d1f18d0bcf986ae5d1dd69c59eda879d897dad
Louis-Lcq <83230080+Louis-Lcq@users.noreply.github.com>
2021-06-07 10:19:39 +0200

    Update graph.js

--- docs/graph.js
+++ docs/graph.js
@@ -1,10 +1,7 @@
 $.getJSON("https://github.com/Louis-Lcq/testGitHubAPI/blob/master/docs/assets/data/gama.json", function(data) {
-   var labels = data.customers[0].amounts.map(function(e) {
-      return e[0];
+   var labels = [Date()]
    });
-   var data = data.customers[0].amounts.map(function(e) {
-      return e[1];
-   });
+   var data = [100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500]
 
    var ctx = document.getElementById('myChart').getContext('2d');
    var chart = new Chart(ctx, {
